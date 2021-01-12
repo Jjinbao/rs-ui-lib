61f20c8962ab5ba6f4ccf41b3a4992588800e9a4
jinbao <jinbaow@yeah.net>
2021-01-12 15:49:46 +0800

    start

---
+++ packages/Button/src/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="x-button">
+        <div>我是一个大大的按钮</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'x-button',
+  props: {
+    type: String
+  }
+}
+</script>
+<style scoped>
+    .x-button {
+      display: inline-block;
+      padding: 3px 6px;
+      background: green;
+      color: #fff;
+    }
+</style>
